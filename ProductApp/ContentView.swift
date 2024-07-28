6e31ab93ebca4ba33c489af8be5b374dbb95a4e6
singhpriti <singhpriti281@gmail.com>
2024-07-28 11:31:33 +0530

    Initial Commit

---
+++ ProductApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProductApp
+//
+//  Created by Preity Singh on 28/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProductApp/ProductAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductAppApp.swift
+//  ProductApp
+//
+//  Created by Preity Singh on 28/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ProductAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
